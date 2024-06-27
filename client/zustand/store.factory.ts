a581d1bd4d370229b6463e8cd79d629ceb5df573
Arno <surfacew@163.com>
2024-06-27 14:30:06 +0800

    :bug: (zustand-factory) add memo & local map clean callback

--- client/zustand/store.factory.ts
+++ client/zustand/store.factory.ts
@@ -1,12 +1,18 @@
 import { create } from 'zustand';
 
-export const createZustandStoreFactory = <T>(storeCreateFn: (uid: string) => ReturnType<typeof create>) => {
+export const createZustandStoreFactory = <T>(storeCreateFn: (uid: string, storeFn: {
+  destroy: () => void;
+}) => ReturnType<typeof create>) => {
   const localMap = new Map<string, ReturnType<ReturnType<typeof create>>>();
   return (uid: string) => {
     if (localMap.has(uid)) {
       return localMap.get(uid) as T;
     }
-    localMap.set(uid, storeCreateFn(uid));
+    localMap.set(uid, storeCreateFn(uid, {
+      destroy: () => {
+        localMap.delete(uid);
+      }
+    }));
     return localMap.get(uid) as T;
   }
 }
