8019a9281af19f71b7c6e12d7b71d1b1cc9d1b46
Arno <surfacew@163.com>
2025-07-08 16:47:16 +0800

    🔧 chore: remove unused ServerComponentDemo and SharedComponentDemo components from the project

--- ui/server/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const ServerComponentDemo = () => {
-  return <div>Server Component Demo</div>;
-};
--- ui/shared/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const SharedComponentDemo = () => {
-  return <div>Shared Component Demo</div>;
-};
